3b4f43a89eae71f9e15564e440641364fdbe115d
tuandh12345 <129180520+tuandh12345@users.noreply.github.com>
2025-05-19 10:40:04 +0700

    update

--- assets/js/app/app.js
+++ assets/js/app/app.js
@@ -218,7 +218,7 @@ app
     })
     .service('API', function () {
         // const baseUrl = 'https://graduationthesis-production.up.railway.app/api/v1/auth';
-        const baseUrl = 'https://tooth-teeth-dental-clinic-production.up.railway.app/api/v1/auth';
+        const baseUrl = 'https://dental-clinic-912d9bfbae0b.herokuapp.com/api/v1/auth';
         return {
             getBaseUrl: function () {
                 return baseUrl;
