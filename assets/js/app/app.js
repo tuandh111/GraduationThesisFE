0f281906d0866772899b5232e78dc69880e38eb7
tuandh12345 <129180520+tuandh12345@users.noreply.github.com>
2025-05-22 20:00:37 +0700

    update

--- assets/js/app/app.js
+++ assets/js/app/app.js
@@ -218,6 +218,7 @@ app
     })
     .service('API', function () {
         // const baseUrl = 'https://graduationthesis-production.up.railway.app/api/v1/auth';
+        //const baseUrl = 'http://localhost:8080/api/v1/auth';
         const baseUrl = 'https://tooth-teeth-dental-clinic-production.up.railway.app/api/v1/auth';
         return {
             getBaseUrl: function () {
@@ -255,6 +256,7 @@ app
             }
 
             // Connect only if not already connected
+            //socket = new SockJS('http://localhost:8080/api/v1/auth/ws');
             socket = new SockJS('https://tooth-teeth-dental-clinic-production.up.railway.app/api/v1/auth/ws');
             stompClient = Stomp.over(socket);
 
